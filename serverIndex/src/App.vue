8c8331c011b24be50ac4a35c745cc3e74442774b
kaiwen@T <kaiweno425@gmail.com>
2021-06-10 10:09:57 +0800

    init

---
+++ serverIndex/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <h2 class="main-title">我的网站导航</h2>
+  <list :list="showList"></list>
+</template>
+
+<script setup>
+import { reactive } from "@vue/reactivity";
+import List from "./components/List.vue";
+const showList = reactive([
+  {
+    name: "vue音乐",
+    url: "./vuemusic",
+    id: "vuemusic",
+  },
+]);
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.main-title {
+  padding: 20px;
+  background-color: rgb(140, 245, 240);
+}
+</style>
---
+++ serverIndex/src/components/List.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-row :gutter="20" class="list-contain">
+    <el-col :span="6" v-for="item in list" :key="item.id">
+      <el-tag size="medium" @click="goto(item)">{{ item.name }}</el-tag>
+    </el-col>
+  </el-row>
+</template>
+
+<script setup>
+import { defineProps, reactive } from "vue";
+
+defineProps({
+  list: Array,
+});
+const goto = (item) => {
+  console.log("item", item);
+  console.log("item", item.url);
+  window.location.href = item.url;
+};
+</script>
+
+<style scoped>
+.list-contain {
+  padding: 10px;
+}
+</style>
